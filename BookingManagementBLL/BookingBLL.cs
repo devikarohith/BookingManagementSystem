113b84b70f32feb470161a1fe1aec3cae682ba53
devikarohith <devikasoman6@gmail.com>
2026-02-21 05:06:59 +0530

    Initial commit
    Initial commit

---
+++ BookingManagementBLL/BookingBLL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using BookingManagementDAL;
+
+namespace BookingManagementBLL
+{
+    public class BookingBLL
+    {
+        private readonly string connectionstr;
+        public BookingBLL()
+        {
+            connectionstr = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+        }
+        public IEnumerable<Booking> GetAllReservation()
+        {
+            
+            using (var connection = new SqlConnection(connectionstr))
+            {
+                //Set up DynamicParameters object to pass parameters  
+                DynamicParameters parameters = new DynamicParameters();
+
+
+                //Execute stored procedure and map the returned result to a Customer object  
+                var customer = connection.Query<Booking>("GetReservationList", commandType: CommandType.StoredProcedure);
+                return customer.ToList();
+            }
+            
+        }
+    }
+}
---
+++ DevikaBookingManagement/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookingManagementBLL;
+using DevikaBookingManagement.Models;
+
+namespace DevikaBookingManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ReservationList()
+        {
+            BookingModel bookingModel = new BookingModel();
+            BookingBLL obj = new BookingBLL();
+            var lstResult = obj.GetAllReservation();
+            if (lstResult != null)
+            {
+                bookingModel.Bookings = lstResult.Select(item => new BookingModel
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    Amount = item.Amount
+                }).ToList();
+            }
+            
+            return View(bookingModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
